743e590be29df4022ab38fba6a370bbe5932042d
gudkovv <guda20041111@gmail.com>
2025-06-26 13:23:27 +0300

    trying to import Swiper

--- ednipro/src/components/MuseumBox.js
+++ ednipro/src/components/MuseumBox.js
@@ -1,20 +1,31 @@
 import museums from '../data/museums.json';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 function MuseumBox() {
     return (
         <div className="museumBox">
             <div className="mainContainer">
-                <div className="musemBoxContent">
-
-                    <div className="museumBoxGallery">
+                {museums.map((museums) => (
+                    <div className="musemBoxContent" key={museums.id}>
+                        <div className="museumBoxGallery">
+                            <img
+                                src={`/img/${museums.images[0]}`}
+                                alt={museums.name}
+                            />
+                        </div>
+                        <div className="museumBoxText">
+                            <div className="museumBoxTextHeader">
+                                <h2>{museums.name}</h2>
+                                <p>{museums.description}</p>
+                            </div>
+                            <button className="museumBoxButton">Більше інформації</button>
+                        </div>
                     </div>
-
-                    <div className="museumBoxText">
-                        <h2></h2>
-                        <p></p>
-                    </div>
-
-                </div>
+                ))}
             </div>
         </div>
     )
